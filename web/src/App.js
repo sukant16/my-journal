f6878e689d5323211780a9975cca8e3a31185677
Sukant <sukant.iitr@gmail.com>
2021-03-20 08:29:12 +0530

    draft js

--- web/src/App.js
+++ web/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { EditorState, convertToRaw } from 'draft-js';
+import { Editor } from 'react-draft-wysiwyg';
+import { convertToHTML } from 'draft-convert';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [editorState, setEditorState] = useState(
+    () => EditorState.createEmpty(),
+  );
+
+  const [convertedContent, setConvertedContent] = useState("");
+
+  const handleEditorChange = (state) => {
+    setEditorState(state);
+    console.log(convertToRaw(editorState.getCurrentContent()));
+    convertContentToHTML();
+  }
+  
+  const convertContentToHTML = () => {
+    let currentContentAsHTML = convertToHTML(editorState.getCurrentContent());
+    console.log(currentContentAsHTML);
+    setConvertedContent(currentContentAsHTML);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <header className="App-header">
+      Rich Text Editor Example
+    </header>
+    <Editor
+      editorState={editorState}
+      onEditorStateChange={handleEditorChange}
+      wrapperClassName="wrapper-class"
+      editorClassName="editor-class"
+      toolbarClassName="toolbar-class"
+    />
+  </div>
+  )
 }
-
 export default App;
